d359d3893405e7ab4f7c18ce4c7c8a8a2d90175e
xavier-ottolini <xavophonic@yahoo.fr>
2024-05-16 12:01:00 +0200

    change app text

--- src/App.js
+++ src/App.js
@@ -18,6 +18,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <p className="App-intro">
+        I was changed on the <span style="font-style: italic">freature</span> branch!
+      </p>
     </div>
   );
 }
